62865470da9e61b4282fb794f2385a0cad477ad7
Marco Slusalek <m.slusalek@holidaypirates.com>
2020-01-25 13:03:11 +0100

    feat: final part I

---
+++ src/components/LastVueComponent.vue
@@ -0,0 +1,21 @@
+<template>
+<div class="lastvuecomponent">
+<h1>My new Vue Component with the name LastVueComponent</h1>
+</div>
+</template>
+
+
+<script>
+export default {
+name: "LastVueComponent",
+props: {
+msg: String
+},
+methods() {}
+};
+</script>
+
+
+<style scoped>
+.lastvuecomponent {}
+</style>
---
+++ src/components/NewComponent.vue
@@ -0,0 +1,21 @@
+<template>
+<div class="newcomponent">
+<h1>My new Vue Component with the name NewComponent</h1>
+</div>
+</template>
+
+
+<script>
+export default {
+name: "NewComponent",
+props: {
+msg: String
+},
+methods() {}
+};
+</script>
+
+
+<style scoped>
+.newcomponent {}
+</style>
